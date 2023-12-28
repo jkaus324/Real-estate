247b6a827dc59dacd31ff244df40cac97bd764ff
Jatin Kaushal <92943010+jkaus324@users.noreply.github.com>
2023-12-28 21:07:31 +0530

    Made some changes to improve

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,14 +1,15 @@
 import Listing from '../models/listing.model.js';
 import { errorHandler } from '../utils/error.js';
 
-export const createListing = async (req, res, next) => {
+export const createListing = async (req, res) => {
   try {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
   } catch (error) {
-    next(error);
+    console.error('Error creating listing:', error);
   }
 };
+
 
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div className='py-20 px-4 max-w-6xl mx-auto'>
-      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Sahand Estate</h1>
-      <p className='mb-4 text-slate-700'>Sahand Estate is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
-      <p className='mb-4 text-slate-700'>
-      Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Real Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Real Estate is a leading real estate agency that specializes in
+        helping clients buy, sell, and rent properties in the most desirable
+        neighborhoods. Our team of experienced agents is dedicated to providing
+        exceptional service and making the buying and selling process as smooth
+        as possible.
       </p>
-      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+      <p className="mb-4 text-slate-700">
+        Real Estate, founded and led by Jatin Kaushal, is a dynamic and
+        client-focused real estate agency dedicated to facilitating seamless
+        property transactions. Jatin, a full-stack developer, has personally
+        orchestrated the development of this project, combining technical
+        expertise with a profound passion for real estate. Committed to
+        excellence, our experienced team of agents strives to make buying,
+        selling, or renting properties a smooth and rewarding experience. We
+        specialize in navigating the complexities of the real estate market,
+        offering expert advice, personalized service, and in-depth local market
+        knowledge. Our mission is to help clients achieve their real estate
+        goals by providing exceptional service and ensuring that every
+        transaction is exciting and fulfilling. Real Estate is not just about
+        properties; it's about turning dreams into homes, and we are here to
+        guide you every step of the way.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our team of agents has a wealth of experience and knowledge in the real
+        estate industry, and we are committed to providing the highest level of
+        service to our clients. We believe that buying or selling a property
+        should be an exciting and rewarding experience, and we are dedicated to
+        making that a reality for each and every one of our clients.
+      </p>
     </div>
-  )
+  );
 }
